2d87b1b78d4238a26de7308ffe0a42d0239d8045
jude <judezhu.hust@gmail.com>
2014-10-05 14:16:10 -0700

    delete platform android from dev.

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/jude/Projects/Dora/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/jude/Projects/Dora/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/Dorabag-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/jude/Projects/Dora/platforms/android/ant-build/Dorabag-debug-unaligned.apk : \
-/home/jude/Projects/Dora/platforms/android/ant-build/Dorabag.ap_ \
-/home/jude/Projects/Dora/platforms/android/ant-build/classes.dex \
-/home/jude/Projects/Dora/platforms/android/CordovaLib/ant-build/classes.jar \
